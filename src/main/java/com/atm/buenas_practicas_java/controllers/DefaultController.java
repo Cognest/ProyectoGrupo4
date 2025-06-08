20392e58d9e3eb35db2f4d8693993252865c2909
Salvador Bellido <bellidocruz@gmail.com>
2025-06-08 19:12:17 +0200

    - Terminar el diseño de la pantalla de detalles de contenido. - Crear la pantalla plan precios, subir contenido y usuarios bloqueados. - Editar las pantallas de login y signin. - Añadir los controladores de todas las pantallas con algunos ejemplos.

--- src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
@@ -1,13 +1,18 @@
 package com.atm.buenas_practicas_java.controllers;
 
 
+import com.atm.buenas_practicas_java.entities.Prueba;
 import com.atm.buenas_practicas_java.services.EntidadHijaService;
 import com.atm.buenas_practicas_java.services.EntidadPadreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controlador encargado de manejar las solicitudes relacionadas con la entidad principal.
@@ -113,7 +118,84 @@ public class DefaultController {
     @GetMapping("/")
     public String mostrarHome(Model model)
     {
+        List<String> imagenes = List.of(
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+        );
+
+        model.addAttribute("imagenes", imagenes);
         return "index"; // View name
     }
 
+    @GetMapping("/iniciar-sesion")
+    public String mostrarLogin(Model model)
+    {
+        return "login"; // View name
+    }
+
+    @GetMapping("/registrarse")
+    public String mostrarSignin(Model model)
+    {
+        return "signin"; // View name
+    }
+
+    @GetMapping("/imagenes/{id}")
+    public String mostrarImagen(@PathVariable("id") String id, ModelMap interfazConPantalla){
+        System.out.println("Entro en servicios con id");
+        Prueba prueba = new Prueba();
+        prueba.setId(1);
+        prueba.setNum_visitas(1232);
+        prueba.setNum_descargas(145);
+        prueba.setNota(6);
+
+        List<String> imagenes = List.of(
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp",
+                "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+        );
+
+        interfazConPantalla.addAttribute("prueba", prueba);
+        interfazConPantalla.addAttribute("imagen",id);
+        interfazConPantalla.addAttribute("imagenes", imagenes);
+        return "detallesContenido";
+    }
+
+    @GetMapping("/planes")
+    public String mostrarPlanPrecios(Model model)
+    {
+        return "planPrecios"; // View name
+    }
+
+    @GetMapping("/subir-contenido")
+    public String mostrarSubidaContenido(Model model)
+    {
+        return "subirContenido"; // View name
+    }
+
+    @GetMapping("/usuarios-bloqueados")
+    public String mostrarUsuariosBloqueados(Model model)
+    {
+        List<String> imagenes = List.of(
+                "https://randomuser.me/api/portraits/men/99.jpg",
+                "https://randomuser.me/api/portraits/men/1.jpg",
+                "https://randomuser.me/api/portraits/women/2.jpg",
+                "https://randomuser.me/api/portraits/women/99.jpg",
+                "https://randomuser.me/api/portraits/women/1.jpg",
+                "https://randomuser.me/api/portraits/men/2.jpg"
+        );
+
+        model.addAttribute("imagenes", imagenes);
+        return "usuariosBloqueados"; // View name
+    }
 }
