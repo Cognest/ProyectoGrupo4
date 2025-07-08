c582f48868254778564a7b2bbd66df0b1302368b
Felixlagu <felixlaguna2001@gmail.com>
2025-07-08 21:24:02 +0200

    Creacion de la pantalla de busqueda y su controlador

--- src/main/java/com/atm/buenas_practicas_java/controllers/BusquedaCtrl.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/BusquedaCtrl.java
@@ -1,4 +1,4 @@
-package buenas_practicas_java.controllers;
+package com.atm.buenas_practicas_java.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
